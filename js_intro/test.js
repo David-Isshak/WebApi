71017797fdea220eb0dad720356f770b708ea619
tic 95 <david-95200@hotmail.fr>
2024-10-25 14:35:15 +0200

    changement

--- js_intro/test.js
+++ js_intro/test.js
@@ -1,16 +1,24 @@
 let fruits = ['pomme', 'banane', 'orange', 'fraise', 'mangue'];
 
 
-class student {
-    
-    constructor(nom,age,courses) {
-        this.nom = nom
-        this.age = age
-        this.courses = courses
-    }
-}
+let student = {
+    name: "Marie",
+    age: 20,
+    courses: []
+  };
+  
+  student.age = 21; 
+  student.grade = "A"; 
+  
+  student.courses.push("Math", "Physics", "Chemistry");
+  
+  let indexPhysics = student.courses.indexOf("Physics");
+  
 
 
-let student1 = new student("Marie",20,["pomme","fraise"])
-
-console.log(student1.nom)
+  let firstTwoCourses = student.courses.slice(0, 2);
+  
+  console.log("student", student);
+  console.log("index physique' :", indexPhysics);
+  console.log("new tableau :", firstTwoCourses);
+  
